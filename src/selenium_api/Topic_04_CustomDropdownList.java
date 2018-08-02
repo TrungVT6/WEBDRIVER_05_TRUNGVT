79972a0a2454fa4191d1eef60aaed8aa60bf2608
Trungvt6 <tatrung0802@gmail.com>
2018-08-02 22:43:58 +0700

    Update exercise with Angular

--- src/selenium_api/Topic_04_CustomDropdownList.java
+++ src/selenium_api/Topic_04_CustomDropdownList.java
@@ -20,38 +20,47 @@ public class Topic_04_CustomDropdownList {
 	WebDriver driver;
 	WebDriverWait wait;
 	String baseURL1 = "http://jqueryui.com/resources/demos/selectmenu/default.html";
+	String baseURL2 = "https://material.angular.io/components/select/examples ";
 
-	  @BeforeClass
-	  public void beforeClass() {
-//		  driver = new FirefoxDriver();
-		  System.setProperty("webdriver.chrome.driver", ".\\driver\\chromedriver.exe");
-		  driver = new ChromeDriver();
-		  wait   = new WebDriverWait(driver, 30);
-		  driver.manage().window().maximize();
-		  driver.get(baseURL1);
-		  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		 
-	  }
-	
-	  public void selectCustomDropdownList(String dropdown, String listitems,String valueitem) {
-	    driver.findElement(By.xpath(dropdown)).click();
+    public void selectCustomDropdownList(String dropdown, String listitems,String valueitem) throws Exception {
+	    WebElement dropdownElement = driver.findElement(By.xpath(dropdown));
+	    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", dropdownElement);
+	    Thread.sleep(3000);
+	    dropdownElement.click();
 	    List<WebElement> allitems = driver.findElements(By.xpath(listitems));
 	    wait.until(ExpectedConditions.visibilityOfAllElements(allitems));
 	    for(WebElement item : allitems) {
-	      System.out.println(item.getText());
+	      //System.out.println(item.getText());
 	      if(item.getText().equals(valueitem)) {
 	        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", item);
 	        item.click();
 	        break;
 	      }
 	    }
+	  }	
+  @BeforeClass
+  public void beforeClass() {
+  //driver = new FirefoxDriver();
+    System.setProperty("webdriver.chrome.driver", ".\\driver\\chromedriver.exe");
+    driver = new ChromeDriver();
+    wait   = new WebDriverWait(driver, 30);
+	driver.manage().window().maximize();
 	  }
-	    	  
+		  
   @Test
-  public void TC_01_selectCustomDropdownList_jquery() {
-   
+  public void TC_01_selectCustomDropdownList() throws Exception {
+    driver.get(baseURL1);
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     selectCustomDropdownList("//span[@id='number-button']","//ul[@id='number-menu']//li[@class='ui-menu-item']/div", "19");
     Assert.assertTrue(driver.findElement(By.xpath("//span[@id='number-button']//span[@class='ui-selectmenu-text' and text()='19']")).isDisplayed());
+    
+    driver.get(baseURL2);
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	selectCustomDropdownList("//mat-select[@id='mat-select-0']","//mat-option/span", "Pizza");
+    Assert.assertTrue(driver.findElement(By.xpath("//div[@class='mat-select-value']//span[text()='Pizza']")).isDisplayed());
+    
+    selectCustomDropdownList("//mat-select[@id='mat-select-5']","//mat-option/span", "Idaho");
+    Assert.assertTrue(driver.findElement(By.xpath("//div[@class='mat-select-value']//span[text()='Idaho']")).isDisplayed());
 }
   
 
